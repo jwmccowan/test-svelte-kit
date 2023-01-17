725cc28437adb81d08843114c5fb310f55af62e2
jwmccowan <github@jwmccowan.com>
2023-01-17 20:31:53 +1100

    green

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,35 @@
 <script>
+	import Container from "../common/components/container";
 	import "../app.css";
 </script>
 
-<div class="container mx-auto px-4">
-	<nav class="flex flex-row gap-6 py-4">
-		<form method="post" action="/login">
-			<button
-				class="px-4 py-2 text-sm text-white font-medium cursor-default bg-purple-500 rounded hover:bg-purple-600"
-				type="submit">Log in</button
+<div class="bg-green-500">
+	<Container>
+		<nav class="flex flex-row gap-6 py-4">
+			<form method="post" action="/login">
+				<button
+					class="px-4 py-2 text-sm text-white font-medium cursor-default bg-purple-500 rounded hover:bg-purple-600"
+					type="submit"
+				>
+					Log in
+				</button>
+			</form>
+			<form method="post" action="/logout">
+				<button
+					class="px-4 py-2 text-sm text-white font-medium cursor-default bg-purple-500 rounded hover:bg-purple-600"
+					type="submit"
+				>
+					Log out
+				</button>
+			</form>
+			<a
+				class="px-4 py-2 text-sm text-white font-medium bg-purple-500 rounded hover:bg-purple-600"
+				href="/admin"
 			>
-		</form>
-		<form method="post" action="/logout">
-			<button
-				class="px-4 py-2 text-sm text-white font-medium cursor-default bg-purple-500 rounded hover:bg-purple-600"
-				type="submit">Log out</button
-			>
-		</form>
-		<a
-			class="px-4 py-2 text-sm text-white font-medium cursor-default bg-purple-500 rounded hover:bg-purple-600"
-			href="/admin"
-		>
-			Admin page
-		</a>
-	</nav>
+				Admin page
+			</a>
+		</nav>
+	</Container>
 </div>
 
 <slot />
