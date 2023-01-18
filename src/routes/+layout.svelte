e6d307ce0551df0befa697a32f589aa18c04579d
jwmccowan <github@jwmccowan.com>
2023-01-18 12:18:26 +1100

    remove old buttons and redisign home

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,25 +5,17 @@
 
 <div class="bg-green-500">
 	<Container>
-		<nav class="flex flex-row gap-6 py-4">
-			<form method="post" action="/login">
-				<input name="id" value="12345" hidden />
-				<button
-					class="px-4 py-2 text-sm text-white font-medium cursor-default bg-purple-500 rounded hover:bg-purple-600"
-					type="submit"
-				>
-					Log in - admin
-				</button>
-			</form>
-			<form method="post" action="/login">
-				<input name="id" value="23456" hidden />
-				<button
-					class="px-4 py-2 text-sm text-white font-medium cursor-default bg-purple-500 rounded hover:bg-purple-600"
-					type="submit"
-				>
-					Log in - user
-				</button>
-			</form>
+		<nav class="flex flex-row gap-6 py-4 justify-end">
+			<a
+				class="px-4 py-2 text-sm text-white font-medium bg-purple-500 rounded hover:bg-purple-600 mr-auto"
+				href="/">Home</a
+			>
+			<a
+				class="px-4 py-2 text-sm text-white font-medium bg-purple-500 rounded hover:bg-purple-600"
+				href="/admin"
+			>
+				Admin page
+			</a>
 			<form method="post" action="/logout">
 				<button
 					class="px-4 py-2 text-sm text-white font-medium cursor-default bg-purple-500 rounded hover:bg-purple-600"
@@ -32,12 +24,6 @@
 					Log out
 				</button>
 			</form>
-			<a
-				class="px-4 py-2 text-sm text-white font-medium bg-purple-500 rounded hover:bg-purple-600"
-				href="/admin"
-			>
-				Admin page
-			</a>
 		</nav>
 	</Container>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,49 +11,49 @@
 </svelte:head>
 
 <Container>
-	{#if data.user}
-		<h1 class="text-3xl font-bold py-12">Hello {data.user.email}!</h1>
-	{/if}
-	{#if !data.user}
-		<h1 class="text-3xl font-bold py-12">Hello visitor</h1>
-		<p>Feel free to login</p>
-	{/if}
-	<div class="divide-y divide-green-600 py-8">
-		<div />
-		<div />
+	<div class="divide-y divide-green-300">
+		<section class="py-8">
+			{#if data.user}
+				<h1 class="text-3xl font-bold">Hello {data.user.email}!</h1>
+			{/if}
+			{#if !data.user}
+				<h1 class="text-3xl font-bold">Hello visitor</h1>
+				<p class="mt-6">Feel free to login</p>
+			{/if}
+		</section>
+		<section class="py-8">
+			<h2 class="text-xl font-bold mb-6">Register an email</h2>
+			<p class="mb-6">
+				Do not put in a real email, or one you're not comfortable being posted on this site
+			</p>
+			<form method="post" action="/register">
+				<label class="inline-block mb-1">
+					Email:
+					<input class="px-4 py-2 text-sm border rounded" name="email" type="email" />
+				</label>
+				<button
+					class="px-4 py-2 text-sm text-white font-medium cursor-default bg-purple-500 rounded hover:bg-purple-600"
+					type="submit"
+				>
+					Register
+				</button>
+			</form>
+		</section>
+		<section class="py-8">
+			<h2 class="text-xl font-bold mb-6">Users</h2>
+			<ul class="space-y-4">
+				{#each data.users as user}
+					<li>
+						<form method="post" action="/login">
+							<input name="email" value={user.email} hidden />
+							<button
+								class="px-4 py-2 text-sm text-white font-medium cursor-default bg-purple-500 rounded hover:bg-purple-600"
+								type="submit">Login as {user.email}</button
+							>
+						</form>
+					</li>
+				{/each}
+			</ul>
+		</section>
 	</div>
-	<h2 class="text-xl font-bold pb-8">Register an email</h2>
-	<p class="pb-4">
-		Do not put in a real email, or one you're not comfortable being posted on this site
-	</p>
-	<form method="post" action="/register">
-		<label class="inline-block mb-1">
-			Email:
-			<input class="px-4 py-2 text-sm border rounded" name="email" type="email" />
-		</label>
-		<button
-			class="px-4 py-2 text-sm text-white font-medium cursor-default bg-purple-500 rounded hover:bg-purple-600"
-			type="submit"
-		>
-			Register
-		</button>
-	</form>
-	<div class="divide-y divide-green-600 py-8">
-		<div />
-		<div />
-	</div>
-	<h2 class="text-xl font-bold pb-8">Users</h2>
-	<ul class="space-y-4">
-		{#each data.users as user}
-			<li>
-				<form method="post" action="/login">
-					<input name="email" value={user.email} hidden />
-					<button
-						class="px-4 py-2 text-sm text-white font-medium cursor-default bg-purple-500 rounded hover:bg-purple-600"
-						type="submit">Login as {user.email}</button
-					>
-				</form>
-			</li>
-		{/each}
-	</ul>
 </Container>
